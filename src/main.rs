61ea7b3efdea0d634b871687585532d03c85909d
GodlyJaaaj <sebastien.lucas974@gmail.com>
2025-02-09 18:09:03 +0100

    feat: instant write

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use bsq_rs::file_to_string;
-use std::time::Instant;
-use std::env;
+use std::{env, io};
+use std::io::Write;
 
 pub const MAX_SIZE: usize = 100020000;
 
 fn main() {
-    let start = Instant::now();
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
     let mut buffer = [0u8; MAX_SIZE];
     let (rows, cols, bytes_to_skip) = file_to_string(&mut buffer, filename);
 
-    let duration = start.elapsed();
-    eprintln!("Duration: {:?} {:?} {:?} {:?}", duration, rows, cols, bytes_to_skip);
-
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write_all(&buffer).unwrap();
     //let Some(square) = get_biggest_square(&grid, rows, cols) else {
     //    eprintln!("Error: No square found");
     //    exit(1);
     //};
     //
     //replace_and_display_square(&mut grid, rows, cols, &square);
-    //
 }
